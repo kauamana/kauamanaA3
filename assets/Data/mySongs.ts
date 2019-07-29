d27f7b533f2a3d9cec296228bd95e34f7196d3b7
kauamana <53412681+kauamana@users.noreply.github.com>
2019-07-28 22:39:45 -0400

    Add files via upload

---
+++ assets/Data/mySongs.ts
@@ -0,0 +1,32 @@
+import {songInfo} from '../../app/songInfo' ;
+
+export const MYSONGS:songInfo[]=[
+    {
+        songName:'Coca Cola',
+        artist:'Tony Kakkar & Neha Kakkar',
+        genre:'Pop',
+        year:'2018',
+        picture:'./assets/images/cocaCola.jpg'
+    },
+    {
+        songName:'Vaaste', 
+        artist:'Dhvani Bhanushali & Nikhil DSouza',
+        genre:'Indian Pop',
+        year:'2017',
+        picture:'./assets/images/vaaste.jpg'
+    },
+    {
+        songName:'O Saki Saki',
+        artist:'Neha Kakkar',
+        genre:'Classical',
+        year:'2019',
+        picture:'./assets/images/oSakiSaki.jpg'
+    },
+    {
+        songName:'Slow Motion', 
+        artist:'Shreya Ghoshal',
+        genre:'Contemporary R&B ',
+        year:'2015',
+        picture:'./assets/images/slowMotion.jpg'
+    }
+] ;
